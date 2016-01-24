dc472871869882f8910730880cf139aa0b52ed38
jycLin <thejamielin@gmail.com>
2016-01-25 00:05:53 +0800

    update both scripts and plots

--- plot2.R
+++ plot2.R
@@ -10,14 +10,8 @@ rm("usedata")
 Sys.setlocale("LC_TIME", "English")
 mytime <- strptime(paste(dt$Date, dt$Time), "%d/%m/%Y %H:%M:%S",
                    tz = "America/New_York")
-myxorigin = as.numeric(mytime[1])
-secinday = 24 * 60 * 60
-xlabat = myxorigin + secinday * 0:2
-xlabels = weekdays(as.Date(as.POSIXct(xlabat, origin = '1970-01-01', 
-                                      tz = "America/New_York")),TRUE)
 png(file="plot2.png",width=480,height=480, units = "px", bg = "transparent")
-plot(as.numeric(mytime), dt$Global_active_power, 
+plot(mytime, dt$Global_active_power, 
      ylab = "Global Active Power (kilowatts)",
-     xaxt = "n", type = "l", xlab = "", cex.axis = 1, cex.lab = 1)
-axis(1, at = xlabat, labels = xlabels, cex.axis = 1)
+     type = "l", xlab = "", cex.axis = 1, cex.lab = 1)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -10,18 +10,12 @@ rm("usedata")
 Sys.setlocale("LC_TIME", "English")
 mytime <- strptime(paste(dt$Date, dt$Time), "%d/%m/%Y %H:%M:%S",
                    tz = "America/New_York")
-myxorigin = as.numeric(mytime[1])
-secinday = 24 * 60 * 60
-xlabat = myxorigin + secinday * 0:2
-xlabels = weekdays(as.Date(as.POSIXct(xlabat, origin = '1970-01-01', 
-                                      tz = "America/New_York")),TRUE)
 png(file="plot3.png",width=480,height=480, units = "px", bg = "transparent")
-plot(as.numeric(mytime), dt$Sub_metering_1, ylab = "Energy sub metering",
-     xaxt = "n", type = "l", xlab = "", cex.axis = 1, cex.lab = 1)
-lines(as.numeric(mytime), dt$Sub_metering_2, col = "red")
-lines(as.numeric(mytime), dt$Sub_metering_3, col = "blue")
+plot(mytime, dt$Sub_metering_1, ylab = "Energy sub metering",
+     type = "l", xlab = "", cex.axis = 1, cex.lab = 1)
+lines(mytime, dt$Sub_metering_2, col = "red")
+lines(mytime, dt$Sub_metering_3, col = "blue")
 legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        cex = 1, x.intersp = 2.5, adj = 0.2)
-axis(1, at = xlabat, labels = xlabels, cex.axis = 1)
 dev.off()
